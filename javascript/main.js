298c91984f1d132f910d49ae1c2d33fc58856f53
mylu <maxlu@college.harvard.edu>
2014-12-12 06:47:24 -0500

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,12 +1,10 @@
 function showCatArt() {
     html = "<img src=\"/images/score_dist_by_year/Arts_scoredist.jpg\" alt=\"Arts Score Dist\">"
     $('#cat_dist').html(html);
-    clearContactInfo();
 }
 function showCatClothing() {
     html = "<img src=\"/images/score_dist_by_year/Clothing_&_Accessories_scoredist.jpg\" alt=\"Clothing Score Dist\">"
     $('#cat_dist').html(html);
-    clearContactInfo();
 }
          
 
@@ -15,6 +13,7 @@ function showCatClothing() {
  */
 $(document).ready(function () {
     // assigns functions that are executed when a button is clicked
+    $('#cat_dist').html('hello');
     $('#cat_art').click(showCatArt);
     $('#cat_clothing').click(showCatClothing);
     $('#cat_electronics').click(showCatElectronics);
