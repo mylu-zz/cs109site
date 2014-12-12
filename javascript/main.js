f4adec6263eb74f7fccb053093a619f6ed09f1b6
mylu <maxlu@college.harvard.edu>
2014-12-12 14:59:38 -0500

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,3 @@
-
 function showCatArt() {
     $('#cat_dist').html('');
     html = "<img src=\"images/scoredists/Arts_scoredist.jpg\" alt=\"Arts Score Dist\">";
@@ -14,24 +13,24 @@ function showCatElectronics() {
     html = "<img src=\"images/scoredists/Electronics_scoredist.jpg\" alt=\"Electronics Score Dist\">";
     $('#cat_dist').html(html);
 }
-
-function showSlideShow() {
+function showMAArt() {
     $('#time_corr').html('');
     html = "<div id=\"myCarousel\" class=\"carousel slide\"><div class=\"carousel-inner\" width=\"100%\" height=\"100%\">";
-    html += "<div class=\"item\"><img src=\"http://www.trexglobal.com/property-management/wp-content/uploads/2010/02/real-estate-investment-property-canada.jpg\"/>";
-    html += "</div><div class=\"item\"><img src=\"http://news.worldwidepropertydevelopments.com/wp-content/uploads/2008/09/feature.jpg\"/>";
-    html += "</div><div class=\"item\"><img src=\"http://images.realestate.com/re/homepage/theme4/2.jpg\" />";
+    html += "<div class=\"item\"><img src=\"images/ma_analysis/Arts/Arts_ma_analysis_B0000CBK1L.jpg\"/>";
+    html += "</div><div class=\"item\"><img src=\"images/ma_analysis/Arts/Arts_ma_analysis_B000EOX28E.jpg\"/>";
+    html += "</div><div class=\"item\"><img src=\"images/ma_analysis/Arts/Arts_ma_analysis_B000F7DPEQ.jpg\" />";
+    html += "</div><div class=\"item\"><img src=\"images/ma_analysis/Arts/Arts_ma_analysis_B000JQM1DE.jpg\" />";
+    html += "</div><div class=\"item\"><img src=\"images/ma_analysis/Arts/Arts_ma_analysis_B000NK5VY2.jpg\" />";
+    html += "</div><div class=\"item\"><img src=\"images/ma_analysis/Arts/Arts_ma_analysis_B0002KHBS2.jpg\" />";
     html += "</div></div><a class=\"carousel-control left\" href=\"#myCarousel\" data-slide=\"prev\">‹</a>"; 
     html += "<a class=\"carousel-control right\" href=\"#myCarousel\" data-slide=\"next\">›</a></div>";
-    $('#time_corr').html('test');
-/*
- * Executed when page loads
- */
+    $('#time_corr').html(html);
+}
+
 $(document).ready(function () {
-    // assigns functions that are executed when a button is clicked
     $('#cat_art').click(showCatArt);
     $('#cat_clothing').click(showCatClothing);
     $('#cat_electronics').click(showCatElectronics);
-    $('#ma_art').click(showSlideShow);
-    $('#time_corr').html('test1')
+    $('#ma_art').click(showMAArt);
+    $('ma_clothing').click(showMAClothing);
 });
