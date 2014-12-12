d1ef2853a8b7bc44e1f46ab08ab05a97f95046c4
lindadu <xlindadu@gmail.com>
2014-12-12 16:43:49 -0500

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -70,6 +70,46 @@ function showMAClothing() {
     html += "<a class=\"carousel-control right\" href=\"#myCarousel\" data-slide=\"next\">›</a></div>";
     $('#time_corr').html(html);
 }
+function showMAElectronics() {
+    $('#time_corr').html('');
+    html = "<div id=\"myCarousel\" class=\"carousel slide\"><div class=\"carousel-inner\" width=\"100%\" height=\"100%\">";
+    html += "<div class=\"item\"><img src=\"images/ma_analysis/Electronics/Electronics_ma_analysis_B000BYCKU8.jpg\"/>";
+    html += "</div><div class=\"item\"><img src=\"images/ma_analysis/Electronics/Electronics_ma_analysis_B000BYCKU8.jpg\"/>";
+    html += "</div><div class=\"item\"><img src=\"images/ma_analysis/Electronics/Electronics_ma_analysis_B000IHGJ50.jpg\" />";
+    html += "</div><div class=\"item\"><img src=\"images/ma_analysis/Electronics/Electronics_ma_analysis_B000M2GYF6.jpg\" />";
+    html += "</div><div class=\"item\"><img src=\"images/ma_analysis/Electronics/Electronics_ma_analysis_B0002MQGOK.jpg\" />";
+    html += "</div><div class=\"item\"><img src=\"images/ma_analysis/Electronics/Electronics_ma_analysis_B00008VF63.jpg\" />";
+    html += "</div><div class=\"item\"><img src=\"images/ma_analysis/Electronics/Electronics_ma_analysis_B00063LTSS.jpg\"/>";
+    html += "</div></div><a class=\"carousel-control left\" href=\"#myCarousel\" data-slide=\"prev\">‹</a>"; 
+    html += "<a class=\"carousel-control right\" href=\"#myCarousel\" data-slide=\"next\">›</a></div>";
+    $('#time_corr').html(html);
+}
+function showMAHealth() {
+    $('#time_corr').html('');
+    html = "<div id=\"myCarousel\" class=\"carousel slide\"><div class=\"carousel-inner\" width=\"100%\" height=\"100%\">";
+    html += "<div class=\"item\"><img src=\"images/ma_analysis/Health/Health_ma_analysis_B000A23CQM.jpg\"/>";
+    html += "</div><div class=\"item\"><img src=\"images/ma_analysis/Health/Health_ma_analysis_B000GC1YAC.jpg\"/>";
+    html += "</div><div class=\"item\"><img src=\"images/ma_analysis/Health/Health_ma_analysis_B000JRP2XO.jpg\" />";
+    html += "</div><div class=\"item\"><img src=\"images/ma_analysis/Health/Health_ma_analysis_B000NBQUNW.jpg\" />";
+    html += "</div><div class=\"item\"><img src=\"images/ma_analysis/Health/Health_ma_analysis_B000NL0T1G.jpg\" />";
+    html += "</div><div class=\"item\"><img src=\"images/ma_analysis/Health/Health_ma_analysis_B0009MFUOA.jpg\" />";
+    html += "</div></div><a class=\"carousel-control left\" href=\"#myCarousel\" data-slide=\"prev\">‹</a>"; 
+    html += "<a class=\"carousel-control right\" href=\"#myCarousel\" data-slide=\"next\">›</a></div>";
+    $('#time_corr').html(html);
+}
+function showMAHome() {
+    $('#time_corr').html('');
+    html = "<div id=\"myCarousel\" class=\"carousel slide\"><div class=\"carousel-inner\" width=\"100%\" height=\"100%\">";
+    html += "<div class=\"item\"><img src=\"images/ma_analysis/Home_&_Kitchen/Home_&_Kitchen_ma_analysis_B000EPLK6O.jpg\"/>";
+    html += "</div><div class=\"item\"><img src=\"images/ma_analysis/Home_&_Kitchen/Home_&_Kitchen_ma_analysis_B000OUY2QO.jpg\"/>";
+    html += "</div><div class=\"item\"><img src=\"images/ma_analysis/Home_&_Kitchen/Home_&_Kitchen_ma_analysis_B00004YNX3.jpg\" />";
+    html += "</div><div class=\"item\"><img src=\"images/ma_analysis/Home_&_Kitchen/Home_&_Kitchen_ma_analysis_B0006HVVFK.jpg\" />";
+    html += "</div><div class=\"item\"><img src=\"images/ma_analysis/Home_&_Kitchen/Home_&_Kitchen_ma_analysis_B00030CGKY.jpg\" />";
+    html += "</div><div class=\"item\"><img src=\"images/ma_analysis/Home_&_Kitchen/Home_&_Kitchen_ma_analysis_B000246GSE.jpg\" />";
+    html += "</div></div><a class=\"carousel-control left\" href=\"#myCarousel\" data-slide=\"prev\">‹</a>"; 
+    html += "<a class=\"carousel-control right\" href=\"#myCarousel\" data-slide=\"next\">›</a></div>";
+    $('#time_corr').html(html);
+}
 
 $(document).ready(function () {
     $('#cat_art').click(showCatArt);
